95e4bcbba2c77a4c28cfc511ec1fd1b3dd2df708
Julien du Bois <julien.du.bois@starkleytech.com>
2024-06-17 11:22:32 +0400

    sha

--- Formula/tscli.rb
+++ Formula/tscli.rb
@@ -4,15 +4,15 @@ class Tscli < Formula
   version "0.1.0"
 
   # if OS.mac? && Hardware::CPU.intel?
-  #   url "https://github.com/taostat/homebrew-tscli/releases/download/ve6c035543a7ae45226fa2cf9dfece5216aa945a0/tscli-macos-x86_64.tar.gz"
+  #   url "https://github.com/taostat/taostats-cli/releases/download/ve6c035543a7ae45226fa2cf9dfece5216aa945a0/tscli-macos-x86_64.tar.gz"
   #   sha256 "06583bd22e308c0d8a72ccfa113c568206f9c863a33b24859d6d92f9822a9919"
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/taostat/taostats-cli/releases/download/ve6c035543a7ae45226fa2cf9dfece5216aa945a0/tscli-aarch64-apple-darwin.tar.gz"
     sha256 "166aef918ffa1444164629084177a671dcf012edff174620bd2b6777f5413b85"
-  elif OS.linux?
+  elsif OS.linux?
     url "https://github.com/taostat/taostats-cli/releases/download/ve6c035543a7ae45226fa2cf9dfece5216aa945a0/tscli-x86_64-unknown-linux-gnu.tar.gz"
     sha256 "06583bd22e308c0d8a72ccfa113c568206f9c863a33b24859d6d92f9822a9919"
-  elif OS.windows?
+  elsif OS.windows?
     url "https://github.com/taostat/taostats-cli/releases/download/ve6c035543a7ae45226fa2cf9dfece5216aa945a0/tscli-x86_64-pc-windows-msvc.tar.gz"
     sha256 "7F7BA2CE7CD0FC18E7CBF461E8C675F97D23545BDC57F3986AE64E840AF8D06E"
   end
@@ -27,4 +27,3 @@ class Tscli < Formula
     assert_match "tscli", shell_output("#{bin}/tscli --version")
   end
 end
-EOF
