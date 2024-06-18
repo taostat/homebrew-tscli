7e2bca928b0c15ed41f955c94714ba33e99d7e92
GitHub Action <action@github.com>
2024-06-18 07:07:26 +0000

    Update Homebrew formula for tscli version v0.1.1 [skip ci]

--- Formula/tscli.rb
+++ Formula/tscli.rb
@@ -1,19 +1,20 @@
 class Tscli < Formula
   desc "A CLI tool for taostat"
   homepage "https://github.com/taostat/homebrew-tscli"
-  version "0.1.0"
+  version "0.1.1"
 
-  # if OS.mac? && Hardware::CPU.intel?
-  #   url "https://github.com/taostat/homebrew-tscli/releases/download/ve6c035543a7ae45226fa2cf9dfece5216aa945a0/tscli-macos-aarch64.tar.gz"
-  if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/taostat/homebrew-tscli/releases/download/ve6c035543a7ae45226fa2cf9dfece5216aa945a0/tscli-aarch64-apple-darwin.tar.gz"
-    sha256 "166aef918ffa1444164629084177a671dcf012edff174620bd2b6777f5413b85"
+  if OS.mac? && Hardware::CPU.intel?
+    url "https://github.com/taostat/homebrew-tscli/releases/download/v26e62a0ed86ea0eabd9a932942d2f5e67abb7833/tscli-macos-x86_64.tar.gz"
+    sha256 ""
+  elsif OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/taostat/homebrew-tscli/releases/download/v26e62a0ed86ea0eabd9a932942d2f5e67abb7833/tscli-macos-aarch64.tar.gz"
+    sha256 ""
   elsif OS.linux?
-    url "https://github.com/taostat/homebrew-tscli/releases/download/ve6c035543a7ae45226fa2cf9dfece5216aa945a0/tscli-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "06583bd22e308c0d8a72ccfa113c568206f9c863a33b24859d6d92f9822a9919"
+    url "https://github.com/taostat/homebrew-tscli/releases/download/v26e62a0ed86ea0eabd9a932942d2f5e67abb7833/tscli-linux-x86_64.tar.gz"
+    sha256 ""
   elsif OS.windows?
-    url "https://github.com/taostat/homebrew-tscli/releases/download/ve6c035543a7ae45226fa2cf9dfece5216aa945a0/tscli-x86_64-pc-windows-msvc.tar.gz"
-    sha256 "7F7BA2CE7CD0FC18E7CBF461E8C675F97D23545BDC57F3986AE64E840AF8D06E"
+    url "https://github.com/taostat/homebrew-tscli/releases/download/v26e62a0ed86ea0eabd9a932942d2f5e67abb7833/tscli-windows-x86_64.tar.gz"
+    sha256 "4E478F54D28B5362A239DC87DA2E85A6AF634A0A8C986127E5EE7E88B878C0A9"
   end
 
   license "MIT"
