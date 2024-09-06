fb8ca6ac289dcb653815619db22b15bf16441e04
GitHub Action <action@github.com>
2024-09-06 19:12:14 +0000

    Update Homebrew formula for tscli version v0.2.0 [skip ci]

--- Formula/tscli.rb
+++ Formula/tscli.rb
@@ -1,20 +1,20 @@
 class Tscli < Formula
   desc "A CLI tool for taostat"
   homepage "https://github.com/taostat/homebrew-tscli"
-  version "0.1.0"
+  version "0.2.0"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/taostat/homebrew-tscli/releases/download/v0.1.0/tscli-x86_64-apple-darwin.tar.gz"
+    url "https://github.com/taostat/homebrew-tscli/releases/download/v0.2.0/tscli-x86_64-apple-darwin.tar.gz"
     sha256 ""
   elsif OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/taostat/homebrew-tscli/releases/download/v0.1.0/tscli-aarch64-apple-darwin.tar.gz"
+    url "https://github.com/taostat/homebrew-tscli/releases/download/v0.2.0/tscli-aarch64-apple-darwin.tar.gz"
     sha256 ""
   elsif OS.linux?
-    url "https://github.com/taostat/homebrew-tscli/releases/download/v0.1.0/tscli-x86_64-unknown-linux-gnu.tar.gz"
+    url "https://github.com/taostat/homebrew-tscli/releases/download/v0.2.0/tscli-x86_64-unknown-linux-gnu.tar.gz"
     sha256 ""
   elsif OS.windows?
-    url "https://github.com/taostat/homebrew-tscli/releases/download/v0.1.0/tscli-x86_64-pc-windows-msvc.tar.gz"
-    sha256 "807B353DA75033F5E11F99DFE50C929D95731F1D48D7B1164E8683A42FF19C15"
+    url "https://github.com/taostat/homebrew-tscli/releases/download/v0.2.0/tscli-x86_64-pc-windows-msvc.tar.gz"
+    sha256 "139EB634F9412BAC743067FD5177CEA94A6DF98EE8A550C673F6109B69EEAD4A"
   end
 
   license "MIT"
